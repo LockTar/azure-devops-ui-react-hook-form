819799b35966290fe2fda7d9e7c3932433bfeae6
Ralph Jansen <ralph.jansen@outlook.com>
2022-02-28 14:57:24 +0100

    Get typescript working

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,21 @@
-import babel from 'rollup-plugin-babel'
 import resolve from '@rollup/plugin-node-resolve'
 import external from 'rollup-plugin-peer-deps-external'
 import { terser } from 'rollup-plugin-terser'
 import postcss from 'rollup-plugin-postcss'
+import typescript from 'rollup-plugin-typescript2'
+import pkg from './package.json'
 
 export default [
   {
-    input: './src/index.js',
+    input: './src/index.ts',
     output: [
       {
-        file: 'dist/index.js',
+        file: pkg.main,
         format: 'cjs',
       },
       {
-        files: 'dist/index.es.js',
+        file: pkg.module,
         format: 'es',
-        exports: 'named',
       },
     ],
     plugins: [
@@ -23,12 +23,9 @@ export default [
         plugins: [],
         minimize: true,
       }),
-      babel({
-        exclude: 'node_modules/**',
-        presets: ['@babel/preset-react'],
-      }),
       external(),
       resolve(),
+      typescript(),
       terser(),
     ],
   },
